38a388636a86d2c67be51546b5d39b0deb94b02a
isi <isi001122d@gmail.com>
2026-05-22 16:01:56 +0000

    Implement find_user_by method with explicit SQLAlchemy exceptions

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -5,9 +5,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError      # Səhv sütun arqumentləri üçün
-from sqlalchemy.orm.exc import NoResultFound        # Tapılmayan nəticələr üçün
-
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from typing import TypeVar
 from user import Base, User
 
 
@@ -15,58 +15,42 @@ class DB:
     """DB class
     """
 
-    def __init__(self) -> None:
-        """Initialize a new DB instance
-        """
+    def __init__(self):
+        """ constructor """
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self) -> Session:
-        """Memoized session object
-        """
+    def _session(self):
+        """ create a session """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Creates and saves a new user to the database
-        """
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
+        """ save the user to the database and returns a User object """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
         self._session.commit()
-        self._session.refresh(new_user)
-        return new_user
+        return user
 
     def find_user_by(self, **kwargs) -> User:
-        """Finds the first user filtered by arbitrary keyword arguments
-        """
-        try:
-            # Səhv arqument ötürüləndə InvalidRequestError tutmaq üçün try-except qururuq
-            user = self._session.query(User).filter_by(**kwargs).first()
-        except InvalidRequestError:
+        """ found the users table """
+        if kwargs is None:
             raise InvalidRequestError
-
-        # Əgər filtrə uyğun heç bir istifadəçi tapılmasa
+        user = self._session.query(User).filter_by(**kwargs).first()
         if user is None:
             raise NoResultFound
-
         return user
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        """ Updates a user's attributes in the database
-        """
-        # 1. İstifadəçini id-sinə görə tapırıq
-        user = self.find_user_by(id=user_id)
-
-        # 2. Arqumentləri yoxlayırıq və atributları mənimsədirik
+        """ update user attributes """
+        _id = self.find_user_by(id=user_id)
         for key, value in kwargs.items():
-            if not hasattr(user, key):
+            if not hasattr(_id, key):
                 raise ValueError
-            setattr(user, key, value)
-
-        # 3. Dəyişiklikləri bazaya qeyd edirik
+            setattr(_id, key, value)
         self._session.commit()
